e3aae86e374b9ae36ca89981289e3f05801f9fc7
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2021-02-04 18:09:32 -0500

    destructuring the array just to obtain the name entered

--- globalProcess.js
+++ globalProcess.js
@@ -1,3 +1,7 @@
 console.log(process.pid);
 console.log(process.versions.node);
 console.log(process.argv);
+
+const [ , ,firstName, lastName] = process.argv;
+
+console.log(`Your name is ${firstName} ${lastName}`);
