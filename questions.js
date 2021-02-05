7ff554d14aef3c61a7952e375f27af06c7df3319
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2021-02-05 18:44:57 -0500

    created an ask questions function for the console but it doesnt run properly because it is running syncronously

--- questions.js
+++ questions.js
@@ -1,2 +1,15 @@
 process.stdout.write("Hello ");
 process.stdout.write("World \n\n\n");
+
+const questions = [
+    "What is your name?",
+    "What would you rather be doing?",
+    "What is your prefered programming language?"
+];
+
+const ask = (i=0) => {
+    process.stdout.write(`\n\n ${questions[i]}`);
+    process.stdout.write(` > `);
+};
+
+ask();
