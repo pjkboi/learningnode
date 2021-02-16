6b47fc2bc4fc6006598320519b34b0060078fe43
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2021-02-15 22:41:59 -0500

    using readline functions

--- questions.js
+++ questions.js
@@ -1,5 +1,12 @@
 process.stdout.write("Hello ");
 process.stdout.write("World \n\n\n");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 
 const questions = [
     "What is your name?",
@@ -7,19 +14,43 @@ const questions = [
     "What is your prefered programming language?"
 ];
 
-const ask = (i=0) => {
-    process.stdout.write(`\n\n ${questions[i]}`);
-    process.stdout.write(` > `);
-};
+const collectAnswers = (questions, done) => {
+    const answers = [];
+    const [firstQuestion] = questions;
 
-ask();
+    const questionAnswered = answer => {
+        answers.push(answer);
+        if(answers.length < questions.length){
+            rl.question(questions[answers.length], questionAnswered);
 
-const answers = [];
-process.stdin.on("data", data => {
-    answers.push(data.toString().trim());
-    if(answers.length < questions.length){
-        ask(answers.length);
-    }else{
-        process.exit();
+        }else{
+            done(answers);
+        }
     }
-});
+
+    rl.question(firstQuestion, questionAnswered);
+
+}
+
+collectAnswers(questions, answers =>{
+    console.log("Thank you for your answers. ");
+    console.log(answers);
+    process.exit();
+})
+
+// const ask = (i=0) => {
+//     process.stdout.write(`\n\n ${questions[i]}`);
+//     process.stdout.write(` > `);
+// };
+
+// ask();
+
+// const answers = [];
+// process.stdin.on("data", data => {
+//     answers.push(data.toString().trim());
+//     if(answers.length < questions.length){
+//         ask(answers.length);
+//     }else{
+//         process.exit();
+//     }
+// });
