6f78dbfa350c84c050b4d8224eb0943a29cf1389
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2021-02-18 17:39:27 -0500

    improving module with eventemitters

--- questions.js
+++ questions.js
@@ -7,8 +7,12 @@ const questions = [
 ];
 
 
-collectAnswers(questions, answers =>{
+const answerEvents = collectAnswers(questions);
+
+answerEvents.on("answer", answer => console.log(`question answered: ${answer}`));
+
+answerEvents.on("complete", answers =>{
     console.log("Thank you for your answers. ");
     console.log(answers);
     process.exit();
-})
+});
